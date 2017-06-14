de6d989c4a97056f064ef46826907fe5f501d167
clun <cedrick.lunven@gmail.com>
2017-06-14 16:42:22 +0200

    Working

--- generators/app/index.js
+++ generators/app/index.js
@@ -1,19 +1,29 @@
-const chalk = require('chalk');
-const generator = require('yeoman-generator');
-const packagejs = require('../../package.json');
+// --------------------------------------------------------------------------------------------
+// --- FF4j : Feature Flipping for Java
+// --------------------------------------------------------------------------------------------
+const chalk     	= require('chalk');
+const generator 	= require('yeoman-generator');
+const packagejs 	= require('../../package.json');
 
 // Stores JHipster variables
-const jhipsterVar = { moduleName: 'ff4j' };
+const jhipsterVar 	= { moduleName: 'ff4j' };
 
 // Stores JHipster functions
-const jhipsterFunc = {};
+const jhipsterFunc 	= {};
 
-module.exports = generator.extend({
+// Constants
+const TPL 			= 'template';
+const FF4J_VERSION  = '1.6.4';
 
+// Functions available
+module.exports = generator.extend( {
+
+	// ----------------------------------------------------
+    // Use compose Yeoman capability to maje jhipterVar available (like DB)
+    // ----------------------------------------------------
     initializing: {
-        compose() {
-            this.composeWith('jhipster:modules',
-                { jhipsterVar, jhipsterFunc },
+    	compose() {
+            this.composeWith('jhipster:modules',   { jhipsterVar, jhipsterFunc },
                 this.options.testmode ? { local: require.resolve('generator-jhipster/generators/modules') } : null
             );
         },
@@ -22,28 +32,92 @@ module.exports = generator.extend({
             this.log(`Welcome to the ${chalk.bold.yellow('JHipster ff4j')} generator! ${chalk.yellow(`v${packagejs.version}\n`)}`);
         }
     },
-
+    
+    // ----------------------------------------------------
+    // Ask questions to user to set up the generator
+    // ----------------------------------------------------
     prompting() {
         const done = this.async();
         const prompts = [
+        	{    type: 'confirm',
+                 name: 'ff4jInstall',
+                 message: 'Do you want to install FF4j ?',
+                 default: false   },
             {
-                type: 'input',
-                name: 'message',
-                message: 'Please put something',
-                default: 'hello world!'
-            }
+                type: 'list',
+                name: 'ff4jFeatureStore',
+                message: 'Which database would you like to use to store *Features* ?',
+                choices: [
+                  {name: 'Same as Application', value: 'same'},
+                  {name: 'Cassandra', value: 'cassandra'},
+                  {name: 'ElasticSearch', value: 'elastic'},
+                  {name: 'HazelCast', value: 'hazelCast'},
+                  {name: 'HBase', value: 'hbase'},
+                  {name: 'Ignite', value: 'ignite'},
+                  {name: 'MongoDB', value: 'mongo'},
+                  {name: 'Neo4j', value: 'neo4j'},
+                  {name: 'Redis', value: 'neo4j'},
+                  {name: 'SQL', value: 'sql'},
+                  {name: 'XML', value: 'xml'},
+                ],
+                default: 'same' },
+              {
+                  type: 'list',
+                  name: 'ff4jPropertyStore',
+                  message: 'Which database would you like to use to store *Properties* ?',
+                  choices: [
+                    {name: 'Same as Application', value: 'same'},
+                    {name: 'Same as FeatureStore', value: 'featureStore'},
+                    {name: 'Archaius', value: 'cassandra'},
+                    {name: 'Cassandra', value: 'cassandra'},
+                    {name: 'Commons-config', value: 'commonsconf'},
+                    {name: 'Consul', value: 'consul'},
+                    {name: 'ElasticSearch', value: 'elastic'},
+                    {name: 'HazelCast', value: 'hazelCast'},
+                    {name: 'HBase', value: 'hbase'},
+                    {name: 'Ignite', value: 'ignite'},
+                    {name: 'MongoDB', value: 'mongo'},
+                    {name: 'Neo4j', value: 'neo4j'},
+                    {name: 'Redis', value: 'neo4j'},
+                    {name: 'SQL', value: 'sql'},
+                    {name: 'XML', value: 'xml'},
+                  ],
+                  default: 'same'
+                },
+                {
+                    type: 'list',
+                    name: 'ff4jEventRepository',
+                    message: 'Which database would you like to use to store *AuditTrail* ?',
+                    choices: [
+                      {name: 'Same as Application', value: 'same'},
+                      {name: 'Cassandra', value: 'cassandra'},
+                      {name: 'ElasticSearch', value: 'elastic'},
+                      {name: 'HazelCast', value: 'hazelCast'},
+                      {name: 'HBase', value: 'hbase'},
+                      {name: 'Ignite', value: 'ignite'},
+                      {name: 'MongoDB', value: 'mongo'},
+                      {name: 'Neo4j', value: 'neo4j'},
+                      {name: 'Redis', value: 'neo4j'},
+                      {name: 'SQL', value: 'sql'},
+                    ],
+                    default: 'same' },
         ];
-
+        
+        // After prompting, put variables in the context
         this.prompt(prompts).then((props) => {
             this.props = props;
-            // To access props later use this.props.someOption;
-
             done();
         });
     },
-
+    
+    // -----------------------------------------
+    // Copy Files to be used
+    // -----------------------------------------
     writing() {
-        // function to use directly template
+    	
+    	this.log(`Writing()`);
+    	
+    	// Small function to copy template to destination
         this.template = function (source, destination) {
             this.fs.copyTpl(
                 this.templatePath(source),
@@ -51,16 +125,27 @@ module.exports = generator.extend({
                 this
             );
         };
-
-        this.baseName = jhipsterVar.baseName;
-        this.packageName = jhipsterVar.packageName;
-        this.angularAppName = jhipsterVar.angularAppName;
-        this.clientFramework = jhipsterVar.clientFramework;
-        this.clientPackageManager = jhipsterVar.clientPackageManager;
-        const javaDir = jhipsterVar.javaDir;
-        const resourceDir = jhipsterVar.resourceDir;
-        const webappDir = jhipsterVar.webappDir;
-
+        
+        // COPY function
+        this.copyFiles = function (files) {
+            files.forEach( function(file) {
+              jhipsterFunc.copyTemplate(file.from, file.to, 
+            		  file.type? file.type: TPL, this, file.interpolate? { 'interpolate': file.interpolate } : undefined);
+            }, this);
+        };
+        
+        this.baseName 				= jhipsterVar.baseName;
+        this.packageName 			= jhipsterVar.packageName;
+this.angularAppName 		= jhipsterVar.angularAppName;
+        this.clientFramework 		= jhipsterVar.clientFramework;
+        this.clientPackageManager 	= jhipsterVar.clientPackageManager;
+        
+        const javaDir 				= jhipsterVar.javaDir;
+        const resourceDir 			= jhipsterVar.resourceDir;
+        const webappDir 			= jhipsterVar.webappDir;
+        
+        this.javaDir 				= jhipsterVar.javaDir;
+        this.javaTemplateDir 		= 'src/main/java/package';
         this.message = this.props.message;
 
         this.log('\n--- some config read from config ---');
@@ -69,16 +154,40 @@ module.exports = generator.extend({
         this.log(`angularAppName=${this.angularAppName}`);
         this.log(`clientFramework=${this.clientFramework}`);
         this.log(`clientPackageManager=${this.clientPackageManager}`);
+        
         this.log(`javaDir=${javaDir}`);
         this.log(`resourceDir=${resourceDir}`);
         this.log(`webappDir=${webappDir}`);
         this.log(`\nmessage=${this.message}`);
         this.log('------\n');
 
-        this.template('dummy.txt', 'dummy.txt');
+        // Update Dependencies
+        if (jhipsterVar.buildTool === 'maven') {
+            jhipsterFunc.addMavenDependency('org.ff4j', 'ff4j-core', 			     FF4J_VERSION);
+            jhipsterFunc.addMavenDependency('org.ff4j', 'ff4j-web', 				 FF4J_VERSION);
+            jhipsterFunc.addMavenDependency('org.ff4j', 'ff4j-spring-services', 	 FF4J_VERSION);
+            jhipsterFunc.addMavenDependency('org.ff4j', 'ff4j-spring-boot-web-api',  FF4J_VERSION);
+            
+        } else if (jhipsterVar.buildTool === 'gradle') {
+        	jhipsterFunc.addGradleDependency('org.ff4j', 'ff4j-core', 			 	 FF4J_VERSION);
+            jhipsterFunc.addGradleDependency('org.ff4j', 'ff4j-web',  			 	 FF4J_VERSION);
+            jhipsterFunc.addGradleDependency('org.ff4j', 'ff4j-spring-services', 	 FF4J_VERSION);
+            jhipsterFunc.addGradleDependency('org.ff4j', 'ff4j-spring-boot-web-api', FF4J_VERSION);
+            
+        }
+        
+        // Copy Files (Java)
+        files = [
+        	// Register Servlet and FF4J bean
+            { from: this.javaTemplateDir + '/config/ff4j/_FF4jWebConfiguration.java', to: this.javaDir + 'config/ff4j/FF4jWebConfiguration.java'}
+          ];
+          this.copyFiles(files);
+          //jhipsterFunc.addChangelogToLiquibase(this.changelogDate + '_added_entity_EntityAuditEvent');
     },
+    
 
     install() {
+    	this.log(`install()`);
         let logMsg =
             `To install your dependencies manually, run: ${chalk.yellow.bold(`${this.clientPackageManager} install`)}`;
 
@@ -104,6 +213,9 @@ module.exports = generator.extend({
     },
 
     end() {
+    	this.log(`end()`);
         this.log('End of ff4j generator');
     }
 });
+
+
---
+++ generators/app/templates/src/main/java/package/config/ff4j/_FF4jWebConfiguration.java
@@ -0,0 +1,58 @@
+package <%=packageName%>.config.ff4j;
+
+/*
+ * #%L
+ * ff4j-spring-boot-autoconfigure
+ * %%
+ * Copyright (C) 2013 - 2016 FF4J
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+
+import org.ff4j.FF4j;
+import org.ff4j.web.FF4jDispatcherServlet;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by Paul
+ *
+ * @author <a href="mailto:paul58914080@gmail.com">Paul Williams</a>
+ */
+@Configuration
+@ConditionalOnClass({FF4jDispatcherServlet.class})
+public class FF4jWebConfiguration extends SpringBootServletInitializer {
+
+	@Bean
+	public FF4j getFF4j() {
+		return new FF4j();
+	}
+	
+    @Bean
+    public ServletRegistrationBean ff4jDispatcherServletRegistrationBean(FF4jDispatcherServlet ff4jDispatcherServlet) {
+        return new ServletRegistrationBean(ff4jDispatcherServlet, "/ff4j-web-console/*");
+    }
+
+    @Bean
+    @ConditionalOnMissingBean
+    public FF4jDispatcherServlet getFF4jDispatcherServlet(FF4j ff4j) {
+        FF4jDispatcherServlet ff4jConsoleServlet = new FF4jDispatcherServlet();
+        ff4jConsoleServlet.setFf4j(ff4j);
+        return ff4jConsoleServlet;
+    }
+}
